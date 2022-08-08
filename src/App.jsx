e113ef9023a5ff37fc5a21e4586da6959cbdfd61
Furkan Taşdemir <furkantsdmr97@gmail.com>
2022-08-08 11:37:25 +0300

    first-step-completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Intro from './components/intro/Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
