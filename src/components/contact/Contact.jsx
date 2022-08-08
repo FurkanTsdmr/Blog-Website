ecd7e7191c00ab043bd6d60ad74550eb1d769777
Furkan Taşdemir <furkantsdmr97@gmail.com>
2022-08-08 13:40:22 +0300

    finish

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,56 @@
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/addres.png";
+import { useContext, useRef, useState } from "react";
+import { ThemeContext } from "../../context";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false)
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +90 9999999
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              furkantsdmr97@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              TURKEY/GIRESUN
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} >
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+            {done && "Thank you..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
